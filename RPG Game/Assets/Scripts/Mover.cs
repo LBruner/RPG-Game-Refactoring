0a917515e67845b4a207cd3366509e018df3dd39
LBruner <leandrobruner15@gmail.com>
2021-07-09 14:57:20 -0300

    video 6

---
+++ RPG Game/Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent = null;
+    [SerializeField] Transform target = null;
+
+    Ray lastRay;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            MoveToCursor();
+
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if(hasHit)
+            agent.SetDestination(hit.point);
+
+        Debug.DrawRay(ray.origin, ray.direction * Mathf.Infinity);
+
+    }
+}
