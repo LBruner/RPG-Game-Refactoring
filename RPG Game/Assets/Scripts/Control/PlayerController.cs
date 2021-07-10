300b740aac15c6d3d72b1cae4551cc5ffdfe382b
LBruner <leandrobruner15@gmail.com>
2021-07-10 20:24:55 -0300

    video 4

--- RPG Game/Assets/Scripts/Control/PlayerController.cs
+++ RPG Game/Assets/Scripts/Control/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
 
@@ -16,22 +18,48 @@ namespace RPG.Control
 
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+
+                if(target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
         }
 
-
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
                 mover.MoveTo(hit.point);
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
