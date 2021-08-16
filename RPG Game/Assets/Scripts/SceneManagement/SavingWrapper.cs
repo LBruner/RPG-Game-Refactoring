4c53c3cdad4d24f2560c314284a8e1b144582542
LBruner <leandrobruner15@gmail.com>
2021-08-16 14:12:40 -0300

    video 3

---
+++ RPG Game/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaulSaveFile = "save";
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaulSaveFile);
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaulSaveFile);
+        }
+    }
+}
