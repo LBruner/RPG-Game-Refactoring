75d98fc2abd9deb0ace06b350037bacd039ca087
LBruner <leandrobruner15@gmail.com>
2021-08-16 09:39:44 -0300

    video 5

--- RPG Game/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG Game/Assets/Scripts/SceneManagement/Portal.cs
@@ -8,12 +8,21 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = 1;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(this.gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Debug.Log("Print");
+            Destroy(gameObject);
         }
     }
 }
