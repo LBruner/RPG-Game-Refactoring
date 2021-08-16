3b4bf92bc155665e6e7b4843c6ea0f6ffe248caf
LBruner <leandrobruner15@gmail.com>
2021-08-16 11:29:27 -0300

    10

---
+++ RPG Game/Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            Debug.Log("Fadedout");
+            yield return FadeIn(1f);
+            Debug.Log("Fadedin");
+        }
+
+        IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1f)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0f)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
