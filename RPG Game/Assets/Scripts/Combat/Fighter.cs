8e8f714467b69c1dff66a5a2b328b00e26b938de
LBruner <leandrobruner15@gmail.com>
2021-08-17 18:28:13 -0300

    video 4

--- RPG Game/Assets/Scripts/Combat/Fighter.cs
+++ RPG Game/Assets/Scripts/Combat/Fighter.cs
@@ -12,9 +12,9 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
-        [SerializeField] GameObject weaponPrefab = null;
         [SerializeField] Transform handTransform = null;
-        [SerializeField] AnimatorOverrideController weaponOverride = null;
+        [SerializeField] Weapon weapon = null;
+
         private Health target = null;
 
         private float timeSinceLastAttack = Mathf.Infinity;
@@ -22,6 +22,13 @@ namespace RPG.Combat
         void Start()
         {
             SpawnWeapon();
+        }
+
+        private void SpawnWeapon()
+        {
+            if (weapon == null) { return; }
+            Animator animator = GetComponent<Animator>();
+            weapon.Spawn(handTransform, animator);
         }
 
         private void Update()
@@ -40,13 +47,6 @@ namespace RPG.Combat
                 GetComponent<Mover>().Cancel();
                 AttackBehavior();
             }
-        }
-
-        private void SpawnWeapon()
-        {
-            Instantiate(weaponPrefab, handTransform);
-            Animator animator = GetComponent<Animator>();
-            animator.runtimeAnimatorController = weaponOverride;
         }
 
         private void AttackBehavior()
