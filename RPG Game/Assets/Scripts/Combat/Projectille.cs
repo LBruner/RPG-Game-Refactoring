e6dff3a8aae36a6f13852060f4534544fc3d274f
LBruner <leandrobruner15@gmail.com>
2021-08-18 11:50:09 -0300

    video 20

--- RPG Game/Assets/Scripts/Combat/Projectille.cs
+++ RPG Game/Assets/Scripts/Combat/Projectille.cs
@@ -11,7 +11,9 @@ namespace RPG.Combat
         [SerializeField] float speed = 5f;
         [SerializeField] bool isSeeker = false;
         [SerializeField] GameObject hitEffect = null;
-
+        [SerializeField] float maxLifeTime = 10f;
+        [SerializeField] GameObject[] destroyOnHit;
+        [SerializeField] float lifeAfterImpact = 2f;
         float damage = 0;
         Health target = null;
 
@@ -34,6 +36,8 @@ namespace RPG.Combat
         {
             this.target = target;
             this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
         }
 
         private Vector3 GetAimLocation()
@@ -54,10 +58,17 @@ namespace RPG.Combat
 
             target.TakeDamage(damage);
 
+            speed = 0;
+
             if (hitEffect != null)
                 Instantiate(hitEffect, GetAimLocation(), transform.rotation);
 
-            Destroy(gameObject);
+            foreach (GameObject toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, lifeAfterImpact);
         }
     }
 }
