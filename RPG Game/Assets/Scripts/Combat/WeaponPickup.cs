39f92e267afe393491fb25323746778b9a37b960
LBruner <leandrobruner15@gmail.com>
2021-08-19 11:28:01 -0300

    video 24

--- RPG Game/Assets/Scripts/Combat/WeaponPickup.cs
+++ RPG Game/Assets/Scripts/Combat/WeaponPickup.cs
@@ -6,7 +6,15 @@ namespace RPG.Combat
 {
     public class WeaponPickup : MonoBehaviour
     {
+        [SerializeField] BoxCollider boxCollider = null;
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
+
+        private void Start()
+        {
+            foreach (Transform child in transform)
+                print("Foreach loop: " + child);
+        }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -14,7 +22,24 @@ namespace RPG.Combat
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
 
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            boxCollider.enabled = shouldShow;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
