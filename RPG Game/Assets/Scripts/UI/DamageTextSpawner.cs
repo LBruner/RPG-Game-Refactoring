9d64773a3e91a5d45a3e625721ebb22286d4e8fd
LBruner <leandrobruner15@gmail.com>
2021-08-27 12:57:29 -0300

    video 11

---
+++ RPG Game/Assets/Scripts/UI/DamageTextSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        private void Start()
+        {
+            SpawnDamageText(5);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+                SpawnDamageText(5);
+        }
+
+        private void SpawnDamageText(float damage)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+        }
+    }
+}
