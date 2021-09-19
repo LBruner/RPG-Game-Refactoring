82461562ace9b3e71e615077911138f3ede1a307
LBruner <leandrobruner15@gmail.com>
2021-09-19 12:05:20 -0300

    video 6

--- RPG Game/Assets/Scripts/UI/DialogueUI.cs
+++ RPG Game/Assets/Scripts/UI/DialogueUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Dialogue;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPG.UI
 {
@@ -10,12 +11,28 @@ namespace RPG.UI
     {
         [SerializeField] TextMeshProUGUI AIText;
         PlayerConversant playerConversant;
+        [SerializeField] Button nextButton;
+
 
         private void Start()
         {
             playerConversant = GameObject.FindWithTag("Player").GetComponent<PlayerConversant>();
 
+            nextButton.onClick.AddListener(Next);
+
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
             AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
+        }
+
+        private void Next()
+        {
+            playerConversant.Next();
+            UpdateUI();
         }
     }
 }
